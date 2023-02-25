11a98d0664b571917db27fe86f9372f2bc5a6e53
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-02-25 14:34:58 +0100

    123

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,20 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+
+// import { fileURLToPath, URL } from 'node:url'
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/hyrule-jobs',
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
